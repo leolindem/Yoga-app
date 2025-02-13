a82ee303575134c80b3a8697ead1afcc2f0826b1
Leonardo Carneiro Lindemberg <leolind@umich.edu>
2025-02-13 18:52:06 -0500

    workout card format

--- components/WorkoutCard.tsx
+++ components/WorkoutCard.tsx
@@ -1,40 +1,58 @@
 import { ThemedText } from "./ThemedText";
 import { ThemedView } from "./ThemedView";
-import { StyleSheet, ViewStyle, StyleProp, Image } from "react-native";
-
+import {
+  StyleSheet,
+  ViewStyle,
+  StyleProp,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Link, Href } from "expo-router";
 
 type WorkoutCardProps = {
   style?: StyleProp<ViewStyle>;
   title: string;
   time: string;
-}
+  pathname: string;
+};
 
-export function WorkoutCard({style, title, time} : WorkoutCardProps){
-    return (
-      <>
-        <ThemedView style={styles.card}>
-          <Image source={require('@/assets/images/pose1.png')} style={styles.image}/>
-          <ThemedText>{title}</ThemedText>
-          <ThemedText>{time}</ThemedText>
-        </ThemedView>
-      </>
-    );
+export function WorkoutCard({
+  style,
+  title,
+  time,
+  pathname,
+}: WorkoutCardProps) {
+  return (
+    <>
+      <Link href={pathname as Href} asChild>
+        <TouchableOpacity activeOpacity={0.7}>
+          <ThemedView style={styles.card}>
+            <Image
+              source={require("@/assets/images/pose1.png")}
+              style={styles.image}
+            />
+            <ThemedText>{title}</ThemedText>
+            <ThemedText>{time}</ThemedText>
+          </ThemedView>
+        </TouchableOpacity>
+      </Link>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
   card: {
-    alignItems: 'center',
+    alignItems: "center",
     margin: 20,
     padding: 10,
-    borderColor: 'white',
+    borderColor: "white",
     borderWidth: 1,
     borderRadius: 10,
   },
   image: {
     width: 150,
     height: 80,
-    resizeMode: 'cover',
+    resizeMode: "cover",
     borderRadius: 10,
-  }
-
-})
+  },
+});
