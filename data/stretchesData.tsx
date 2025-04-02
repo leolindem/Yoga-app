a0d139780aa453aeab20a71cd040bee871b0a3d0
Leonardo Carneiro Lindemberg <leolind@umich.edu>
2025-04-02 19:11:54 -0400

    new stretches added

--- data/stretchesData.tsx
+++ data/stretchesData.tsx
@@ -1,14 +1,18 @@
 const workoutDetails: Record<string, any> = {
   ["Calf Stretch"]: require("@/assets/images/calf_stretch.png"),
-  ["Hamstring"]: require("@/assets/images/lunge.png"),
-  ["forward fold"]: require("@/assets/images/lunge.png"),
-  ["Piriformis "]: require("@/assets/images/lunge.png"),
-  ["Hip Adductors"]: require("@/assets/images/lunge.png"),
-  ["Lunging Hip Flexor"]: require("@/assets/images/lunge.png"),
-  ["Kneeling Side Bend"]: require("@/assets/images/lunge.png"),
-  ["Happy Baby"]: require("@/assets/images/lunge.png"),
-  ["Frog Stretch"]: require("@/assets/images/lunge.png"),
-  ["Sphinx Pose"]: require("@/assets/images/lunge.png"),
+  ["Hamstring"]: require("@/assets/images/hamstring.png"),
+  ["forward fold"]: require("@/assets/images/forward_bend.png"),
+  ["Piriformis"]: require("@/assets/images/piriformis.png"),
+  ["Child Pose"]: require("@/assets/images/child_pose.png"),
+  ["Standing Calf"]: require("@/assets/images/standing_calf_stretch.png"),
+  ["Lunging Hip Flexor"]: require("@/assets/images/lunging_hip_flexor.png"),
+  ["Happy Baby"]: require("@/assets/images/happy_baby.png"),
+  ["Frog Stretch"]: require("@/assets/images/frog_pose.png"),
+  ["Sphinx Pose"]: require("@/assets/images/sphinx.png"),
+  ["Leg Swings"]: require("@/assets/images/leg_swings.png"),
+  ["High Knees"]: require("@/assets/images/high_knees.png"),
+  ["Figure 4"]: require("@/assets/images/figure_4.png"),
+  ["Butt Kicks"]: require("@/assets/images/butt_kicks.png"),
 };
 
 export default workoutDetails;
